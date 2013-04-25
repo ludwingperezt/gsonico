c8e6dae9603ff6d94747b95e6a3ef8caf5770647
MarioRValdes <MarioRValdes@9afde6c1-07b6-1838-2944-61ee8a9282ca>
2013-04-25 07:16:23 +0000

    Ya esta lista la reproduccion con letra (si esta disponible)

--- Musica/src/modelos/Letra.java
+++ Musica/src/modelos/Letra.java
@@ -22,14 +22,18 @@ public class Letra {
 	//formato "[mm:ss.xx] LETRA"
 	public void agregarLineaTexto(String linea)
 	{
-		int index=linea.indexOf("]");
-		String t=linea.substring(1, index);
-		long a,b,c;
-		a=Long.parseLong(t.substring(0,1))*60000;
-		b=Long.parseLong(t.substring(3,4))*1000;
-		c=Long.parseLong(t.substring(6,7))*10;
-		tiempo.add(a+b+c);
-		texto.add(linea.substring(index+1)+"\n");		
+		if (linea.length()>9){
+			int index=linea.indexOf("]");
+			String t=linea.substring(1, index);
+			long a,b,c;
+			a=Long.parseLong(t.substring(0,2))*60000;
+			b=Long.parseLong(t.substring(3,5))*1000;
+			c=Long.parseLong(t.substring(6,index-1))*10;
+			tiempo.add(a+b+c);
+			texto.add(linea.substring(index+1)+"\n");		
+		}
+		else
+			texto.add(linea+"\n");			
 	}
 	
 	public String getTodaLetra()
@@ -39,6 +43,23 @@ public class Letra {
 		for (int i=0; i<todoTexto.length;i++)
 			Acum+=todoTexto[i].toString();
 		return Acum;
+	}
+	public String getProximaLinea(float TiempoActual) {
+		// TODO Auto-generated method stub
+		String retorno=null;
+		Object[] todoTiempos=tiempo.toArray();
+		for(int i=1; i<todoTiempos.length;i++)
+		{
+			float i0=Float.parseFloat(todoTiempos[i-1].toString());
+			float i1=Float.parseFloat(todoTiempos[i].toString());
+			if (TiempoActual>=i0)
+					if(TiempoActual<i1)
+					{
+						retorno=texto.get(i-1);
+						break;
+					}
+		}
+		return retorno;
 	}
 
 }
